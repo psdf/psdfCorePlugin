75cb4df8cd09f9ec1b8bde860a371d64d5ef3f01
Juato <juatobaracat@gmail.com>
2010-03-26 17:29:40 -0300

    Se agrega una nueva variable psdf_web_url que hace referencia al proyecto

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
-
+sfConfig::set('psdf_web_url',dirname($_SERVER['SCRIPT_NAME' ]));
 class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
