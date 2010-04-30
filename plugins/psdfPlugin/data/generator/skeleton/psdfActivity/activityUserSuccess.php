368d50cb6211a2b6bcd77841fd5683e21fa440cf
Gustavo Earnshaw <gustavoear@gmail.com>
2010-04-30 19:56:49 -0300

    Se reemplazan los helper (obsoletos de versiones anteriores de symfony) por etiquetas html.

--- plugins/psdfPlugin/data/generator/skeleton/psdfActivity/activityUserSuccess.php
+++ plugins/psdfPlugin/data/generator/skeleton/psdfActivity/activityUserSuccess.php
@@ -1,26 +1,20 @@
 
-<?php use_helper('Form', 'Object', 'Javascript')?>
+<h1>##PROCESS_NAME## > ##ACTIVITY_NAME##</h1>
 
-<div id="sf_admin_container">
-  <div id="sf_admin_content">
+<form method="post" action="<?php echo url_for('##MODULE##/##ACTIVITY##') ?>">
 
-    <h1>##PROCESS_NAME## > ##ACTIVITY_NAME##</h1>
-  
-    <?php echo form_tag("##MODULE##/##ACTIVITY##", array("id"=>"form")) ?>
+    <?php // logica particular del patron si la hay ?>
+    <?php if( isset($include) ): ?>
+        <?php include($include); ?>
+    <?php endif; ?>
 
-		  <?php // logica particular del patron si la hay ?>
-		  <?php if( isset($include) ): ?>
-			  <?php include($include); ?>
-			<?php endif; ?>
-	
-		  <?php echo input_hidden_tag('f', $f->getId()) ?>
-	  
-		  <?php // Acciones de la actividad (continuar, abortar, etc..) ?>
-		  <?php // No deberian expresarse si la actividad se autocompleta sola ?>
-		  <?php if( '0'=='##ACTIVITY_AUTOCOMPLETE##'): ?>
-		    <?php echo submit_tag('Continuar'); ?>
-		  <?php endif; ?>
-	  <?php echo "</form>" ?>
-	  
-  </div>
-</div>      
+    <input type="hidden" name="f" value="<?php echo $f->getId() ?>" />
+
+    <?php // Acciones de la actividad (continuar, abortar, etc..) ?>
+    <?php // No deberian expresarse si la actividad se autocompleta sola ?>
+    <?php if( '0'=='##ACTIVITY_AUTOCOMPLETE##' ): ?>
+        <li>
+            <input type="submit" value="Continuar" />
+        </li>
+    <?php endif; ?>
+<?php echo "</form>" ?>
