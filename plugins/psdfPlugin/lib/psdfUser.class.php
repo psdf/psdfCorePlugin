49f7fbd52fe59ddd14b866b515c2bad63d85836c
Gustavo Earnshaw <gustavoear@gmail.com>
2010-06-14 23:47:45 -0300

    La clase psdfUser ahora hereda de sfGuardSecurityUser (clase del plugin sfDoctrineGuard)

--- plugins/psdfPlugin/lib/psdfUser.class.php
+++ plugins/psdfPlugin/lib/psdfUser.class.php
@@ -1,7 +1,16 @@
 <?php
 
-class psdfUser extends sfBasicSecurityUser
+/**
+ * Extensión de la clase User para los aplicativos. Aqui se extiende la 
+ * clase previamente extendida por el plugin sfDoctrineGuard:
+ * psdfUser
+ *   sfGuardSecurityUser
+ *     sfBasicSecurityUser
+ */
+class psdfUser extends sfGuardSecurityUser
 {
+  protected $menu;
+
   public function setCurrentFlow(Flow $flow) {
     $flows = $this->getAttribute('flows', array(), 'psdf');
     array_push( $flows, $flow );
@@ -26,6 +35,34 @@ class psdfUser extends sfBasicSecurityUser
     $pattern = array_pop( $patterns);
     $this->setAttribute( 'patterns', $patterns, 'psdf');
     return $pattern;
+  }
+
+  public function setMenu() {
+
+      if( !$this->isAuthenticated() ) {
+        // No hay usuario autenticado asumo el invitado
+        $usr = Doctrine::getTable('sfGuardUser')->findByUsername('invitado');
+        $usr = $usr[0];
+      }
+      else {
+        $usr = $this->getGuardUser();
+      }
+
+      //die('Usuario: '.$usr->getUsername() );
+      
+      // Ojo! el usuario puede tener más de una organizacion
+      // Esto debería ajustarse
+      $usuorgs = Doctrine::getTable('Usuorg')->findByRelUsuario($usr->getId());
+      $menuYml = sfYaml::load( $usuorgs[0]->getMenu() );
+      $this->menu = UtilPsdf::convertMenu( $menuYml );
+  }
+
+  public function getMenu() {
+      // Si aun no está seteado el menu, lo cargo
+      if( !$this->menu ) {
+          $this->setMenu();
+      }
+      return $this->menu;
   }
 
 }
