bbd7ed9a797550a8e59985fd269ef594d6999651
Gustavo Earnshaw <gustavoear@gmail.com>
2010-06-25 15:53:46 -0300

    Modulo temporal para poder editar el menú de un usuario en una organizacion.

---
+++ plugins/psdfPlugin/modules/psdfUsuorg/lib/psdfUsuorgGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * usuorg module configuration.
+ *
+ * @package    sigren
+ * @subpackage usuorg
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class psdfUsuorgGeneratorConfiguration extends BasePsdfUsuorgGeneratorConfiguration
+{
+}
