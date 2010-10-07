d89b01e05a79de7996fb87c10a8e19b66bfdd369
Gustavo Earnshaw <gustavoear@gmail.com>
2010-10-07 08:36:57 -0300

    Nuevos parametros donde el psdf mira los xpdl y xmi. Por defecto será en [project]/data/xpdl y [project]/data/xmi pero pueden redefinirse.

--- config/psdfCorePluginConfiguration.class.php
+++ config/psdfCorePluginConfiguration.class.php
@@ -7,6 +7,7 @@ class psdfCorePluginConfiguration extends sfPluginConfiguration
    */
   public function initialize()
   {
-    
+    sfConfig::set( 'psdf_xpdl_dir', sfConfig::get('sf_data_dir').'/xpdl' );
+    sfConfig::set( 'psdf_xmi_dir', sfConfig::get('sf_data_dir').'/xmi' );
   }
 }
