1870f9091246b6b04b48476ad1f072427f5dd8d1
Gustavo Earnshaw <gustavoear@gmail.com>
2010-10-07 18:41:43 -0300

    Clase abstracta sobre la cual definir metodos a referenciar en las condiciones, en el diseño de un proceso.

---
+++ lib/psdfContext.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the psdfCore package.
+ * (c) 2009-2010 Soltic S.R.L. <contacto@proyecto-psdf.com.ar>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * psdfContext es una clase abstracta con funciones que pueden referenciarse
+ * desde el diseño de un proceso
+ *
+ * @package    psdfCore
+ * @subpackage 
+ * @author     Gustavo Earnshaw <gustavoear@gmail.com>
+ */
+abstract class psdfContext {
+
+}
+
+?>
