0f3e9e59ea03d24c45113cc05375227547ad097d
Gustavo Earnshaw <gustavoear@gmail.com>
2010-10-08 19:53:12 -0300

    Evento final del proceso llama al modulo por defecto del proyecto.

--- lib/task/generator/skeleton/activity/action/EndEventAction.class.php
+++ lib/task/generator/skeleton/activity/action/EndEventAction.class.php
@@ -1,24 +1,25 @@
 <?php
 
-class ##ACTION##Action extends sfAction
-{
-  public function execute($request)
-  {
-    $this->f = $this->getUser()->getCurrentFlow();
+class ##ACTION##Action extends sfAction {
 
-    if( !$this->f )
-      throw new sfException('No se pudo recuperar flujo de ejecucion');
-    if( $this->f->getRelProceso() != ##PROCESS_ID## )
-      throw new sfException('El flujo no se corresponde con el proceso instanciado');
-    if( $this->f->getRelEstado()!=EstadoFlow::ACTIVO )
-      throw new sfException('El flujo ya no se encuentra activo');
+    public function execute($request) {
+
+        // Recupero y Valido identificador del flujo
+        $this->f = $this->getUser()->getCurrentFlow();
+
+        if( !$this->f )
+            throw new sfException('No se pudo recuperar flujo de ejecucion');
+        if( $this->f->getXpdlProcessId() != '##PROCESS_ID##' )
+            throw new sfException('El flujo no se corresponde con el proceso instanciado');
+        if( $this->f->getRelEstado()!=EstadoFlow::ACTIVO )
+            throw new sfException('El flujo ya no se encuentra activo');
     
-    // Cambio estado a completado y grabo
-    $this->f->setRelEstado(EstadoFlow::COMPLETADO);
-    $this->f->save();
+        // Cambio estado a completado y grabo
+        $this->f->setRelEstado(EstadoFlow::COMPLETADO);
+        $this->f->save();
 
-    // De aqui a donde deberia llamar....
-    return sfView::NONE;
+        // De aqui a donde deberia llamar....
+        $this->redirect('default');
   }
 }
 
